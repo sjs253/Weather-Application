a4d102bf446f0bbf74b88cd60b9c7479b548e98e
sjs253 <shreyansh25.shrivastava@gmail.com>
2020-05-20 12:10:50 +0530

    props configured with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,68 @@ import logo from "./logo.svg";
 import "./App.css";
 import CardListContainer from "./components/WeatherCard/CardListContainer";
 
+export interface weatherInterface {
+  monday: {
+    maxTemp: number;
+    minTemp: number;
+    main: string;
+  };
+  tuesday: {
+    maxTemp: number;
+    minTemp: number;
+    main: string;
+  };
+  wednesday: {
+    maxTemp: number;
+    minTemp: number;
+    main: string;
+  };
+  thursday: {
+    maxTemp: number;
+    minTemp: number;
+    main: string;
+  };
+  friday: {
+    maxTemp: number;
+    minTemp: number;
+    main: string;
+  };
+}
+
 function App() {
+  const state: weatherInterface = {
+    monday: {
+      maxTemp: 40,
+      minTemp: 30,
+      main: "sunny",
+    },
+    tuesday: {
+      maxTemp: 35,
+      minTemp: 28,
+      main: "cloudy",
+    },
+    wednesday: {
+      maxTemp: 30,
+      minTemp: 22,
+      main: "rainy",
+    },
+    thursday: {
+      maxTemp: 20,
+      minTemp: 12,
+      main: "thunderstorm",
+    },
+    friday: {
+      maxTemp: 32,
+      minTemp: 28,
+      main: "cloudy",
+    },
+  };
+
   return (
     <div style={{ margin: 16 }}>
       <h1>Weather Application</h1>
       <Fragment>
-        <CardListContainer />
+        <CardListContainer {...state} />
       </Fragment>
     </div>
   );
--- src/components/WeatherCard/CardListContainer.tsx
+++ src/components/WeatherCard/CardListContainer.tsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
 import CardList from "./CardList";
+import { weatherInterface } from "../../App";
 
-function CardListContainer() {
+function CardListContainer(props: weatherInterface) {
   return (
     <Fragment>
       <h2>Here is the daily weather forecast of 5 consecutive days!</h2>
-      <CardList />
+      <CardList {...props} />
     </Fragment>
   );
 }
